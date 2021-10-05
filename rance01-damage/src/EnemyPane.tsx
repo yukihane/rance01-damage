9338202f725583ae3983423582d8bf67a89afaf2
yukihane <yukihane.feather@gmail.com>
2021-10-06 02:22:19 +0900

    敵のペイン

--- rance01-damage/src/EnemyPane.tsx
+++ rance01-damage/src/EnemyPane.tsx
@@ -1,3 +1,22 @@
 export const EnemyPane = () => {
-  return <div>EnemyPane</div>;
+  return (
+    <div>
+      <div>
+        <label>
+          攻<input type="text" name="enemyAtack" id="enemyAtack" />
+        </label>
+      </div>
+      <div>
+        <label>
+          防<input type="text" name="enemyDefense" id="enemyDefense" />
+        </label>
+      </div>
+      <div>
+        <label>
+          ハニーフラッシュ
+          <input type="checkbox" name="honeyFlash" id="honeyFlash" />
+        </label>
+      </div>
+    </div>
+  );
 };
